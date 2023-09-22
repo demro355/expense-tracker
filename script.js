9fecaa12eb5da00e80fc59082c4e893d4102aae5
demro355 <jdemro330@gmail.com>
2023-09-22 18:04:13 +0100

    delete function added

--- script.js
+++ script.js
@@ -7,17 +7,25 @@ function addTransaction() {
     const text = document.getElementById('text').value;
     const amount = parseFloat(document.getElementById('amount').value);
 
+// Create a new list item for the transaction
+const transaction = document.createElement('li');
+transaction.innerHTML = `${text} <span>${amount >= 0 ? '+' : '-'}£${Math.abs(amount).toFixed(2)}</span> <button onclick="removeTransaction(this)">Delete</button>`;
+
+ // Append the transaction to the transactions list
+ transactionsList.appendChild(transaction);
+
+
+
+
+
+
+
 //  Will check if number and text is entered, if not it will throw an error  
     if (text.trim() === '' || isNaN(amount)) {
         alert('Please enter valid text and amount.');
         return;
     }
     
-// New list created to include transsactions
-// Balance updated by adding or subtracting transaction to/from existing amount
-    const transaction = document.createElement('li');
-    transaction.innerHTML = `${text} <span>${amount >= 0 ? '+' : '-'}£${Math.abs(amount).toFixed(2)}</span>`;
-    transactionsList.appendChild(transaction);
 
 // Called to display current balance
     balance += amount;
@@ -30,4 +38,13 @@ the inner text of the balance display element to the
 formatted balance value (fixed to 2 decimal places).*/
 function updateBalance() {
     balanceDisplay.innerText = balance.toFixed(2);
+}
+
+function removeTransaction(transactionElement) {
+    const amountStr = transactionElement.previousElementSibling.innerText; // Extracting the amount string
+    const amount = parseFloat(amountStr.replace('£', '')); // Extracting the amount and converting to a number
+    balance -= amount; // Update the balance by subtracting the amount
+
+    transactionElement.parentElement.remove(); // Remove the transaction element from the DOM
+    updateBalance(); // Update the displayed balance
 }
